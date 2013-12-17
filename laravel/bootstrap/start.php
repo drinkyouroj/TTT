863f8ba3604f3b2b300af29a61c929535c076d38
Ryuhei Yokokawa <ryuhei@y-designs.com>
2013-12-16 18:35:10 -0800

    vagrant stuff

--- laravel/bootstrap/start.php
+++ laravel/bootstrap/start.php
@@ -28,7 +28,8 @@ $app->redirectIfTrailingSlash();
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('your-machine-name'),
+	'vagrant' => array('localhost:4567'),
+	'local' => array('localhost'),
 
 ));
 
