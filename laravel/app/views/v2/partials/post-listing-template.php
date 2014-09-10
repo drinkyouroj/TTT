fb3d9648b03cf7ed0a18488006227b6c7d00b16a
Ryuhei Yokokawa <ryuhei@y-designs.com>
2014-09-10 16:48:47 -0700

    worked on the system

--- laravel/app/views/v2/partials/post-listing-template.php
+++ laravel/app/views/v2/partials/post-listing-template.php
@@ -39,52 +39,54 @@
 			</a>
 			{{post.story_type}} by <a href="{{ site_url }}profile/{{ post.user.username }}"> {{ post.user.username }} </a>
 
-			{{#ifCond feed_type 'repost' }}
-				<div class="post-repost-container">
-					<img class="post-repost-image" src="{{ site_url }}img/icons/repost-single.png">
-					<div class="post-repost-count-container"> x 
-						<span class="post-repost-count">{{ users.length }} </span>
-					</div>
-					<ul class="post-repost-user-dropdown list-unstyled">
-						{{#each users}}
-							<li> 
-								<a href="{{ ../site_url }}profile/{{ this }}"> {{ this }} </a> 
-							</li>
-						{{/each}}
-					</ul>
-				</div>
-			{{/ifCond}}
-			{{#ifCond myprofile true }}
-				{{#ifCond post.user.id user_id }}
-					<div class="options-link"> </div>
-					<div class="post-options">
-						{{#ifCond editable true}}
-							<a class="post-edit" href="{{site_url}}myprofile/editpost/{{post.id}}">
-								Edit
-							</a>
-						{{/ifCond}}
-
-							<a class="post-delete" data-id="{{post.id}}">
-								Delete
-							</a>
-
-						{{#ifNotCond featured_id post.id}}
-							<a class="set-featured" data-id="{{post.id}}">
-								Feature
-							</a>
-						{{/ifNotCond }}
-					</div>
-				{{/ifCond}}
-				{{#ifCond post_type 'repost' }}
-					<div class="post-options">
-						<a class="remove-repost" data-id="{{post.id}}">
-							Remove Repost
-						</a>
-					</div>
-				{{/ifCond}}
-			{{/ifCond}}
+			
 		</div>
 
+		{{#ifCond feed_type 'repost' }}
+			<div class="post-repost-container">
+				<img class="post-repost-image" src="{{ site_url }}img/icons/repost-single.png">
+				<div class="post-repost-count-container"> x 
+					<span class="post-repost-count">{{ users.length }} </span>
+				</div>
+				<ul class="post-repost-user-dropdown list-unstyled">
+					{{#each users}}
+						<li> 
+							<a href="{{ ../site_url }}profile/{{ this }}"> {{ this }} </a> 
+						</li>
+					{{/each}}
+				</ul>
+			</div>
+		{{/ifCond}}
+		{{#ifCond myprofile true }}
+			{{#ifCond post.user.id user_id }}
+				<div class="options-link"> </div>
+				<div class="post-options">
+					{{#ifCond editable true}}
+						<a class="post-edit" href="{{site_url}}myprofile/editpost/{{post.id}}">
+							Edit
+						</a>
+					{{/ifCond}}
+
+						<a class="post-delete" data-id="{{post.id}}">
+							Delete
+						</a>
+
+					{{#ifNotCond featured_id post.id}}
+						<a class="set-featured" data-id="{{post.id}}">
+							Feature
+						</a>
+					{{/ifNotCond }}
+				</div>
+			{{/ifCond}}
+			{{#ifCond post_type 'repost' }}
+				<div class="post-options">
+					<a class="remove-repost" data-id="{{post.id}}">
+						Remove Repost
+					</a>
+				</div>
+			{{/ifCond}}
+		{{/ifCond}}
+		
 		<a class="image-link" href="{{ site_url }}posts/{{ post.alias }}">
 			<div class="top-fade"> </div>
 				<div class="post-image" style="background-image:url('{{ site_url }}uploads/final_images/{{ post.image }}')">
