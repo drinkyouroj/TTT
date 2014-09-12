75fa234f20cdd207aa812b8f2aa43831645c7afe
Ryuhei Yokokawa <ryuhei@y-designs.com>
2014-09-12 12:01:57 -0700

    worked on the forgot scenario. forgot to update a config outside of testing

--- laravel/app/config/app.php
+++ laravel/app/config/app.php
@@ -14,7 +14,7 @@ return array(
 
 	'debug' => true,
 	'stack' => true,//errors automatically redirects to a specified location.
-	'email_send' => true,
+	'email_send' => false,
 
 	/*
 	|--------------------------------------------------------------------------
