1398230c82caf5e46f57c745bddbaf1b2034eeb6
Ryuhei Yokokawa <ryuhei@y-designs.com>
2014-09-17 13:39:39 -0700

    fixed new user email

--- laravel/app/controllers/UserController.php
+++ laravel/app/controllers/UserController.php
@@ -65,13 +65,12 @@ class UserController extends BaseController {
             $user = $this->user->login($data);
             //Gotta send out email
             if(!empty($data['email']) ) {
-                $confirm = $user->confirmation_code;
                 $email_data = array(
                     'from' => 'no_reply@twothousandtimes.com',
                     'to' => array($data['email']),
                     'subject' => 'Thanks for Joining Two Thousand Times!',
-                    'plaintext' => View::make('v2/emails/new_user_plain')->with('confirm', $confirm)->with('user', $user)->render(),
-                    'html'  => View::make('v2/emails/new_user_html')->with('confirm', $confirm)->with('user', $user)->render()
+                    'plaintext' => View::make('v2/emails/new_user_plain')->with('user', $user)->render(),
+                    'html'  => View::make('v2/emails/new_user_html')->with('user', $user)->render()
                     );
 
                 $this->email->create($email_data);
