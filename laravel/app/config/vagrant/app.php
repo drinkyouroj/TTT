ab60d094be3ce1d8e4ec52d20c4b2275f0125461
Ryuhei Yokokawa <ryuhei@y-designs.com>
2014-09-24 21:03:15 +0000

    edited wordbreak issue and styled for better mobile reading

--- laravel/app/config/vagrant/app.php
+++ laravel/app/config/vagrant/app.php
@@ -4,5 +4,6 @@ return array(
 	'url' => 'http://192.168.9.140:4567',
 	'imageurl' => 'http://192.168.9.140:4567/uploads/final_images',
 	'staticurl' => '//192.168.9.140:4567',
-	'email_send' => true
+	'email_send' => true,
+	'enable_ssl' => false,//enables SSL force on the user and myprofile routes.
 );
