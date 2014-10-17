9fe9cc96747867acb7ee830713dce58a893e6b9c
Ryuhei Yokokawa <ryuhei@y-designs.com>
2014-10-17 15:12:04 -0700

    worked on the category name change system

--- laravel/public/js/v2/admin/admin.js
+++ laravel/public/js/v2/admin/admin.js
@@ -5,7 +5,9 @@ $(function() {
     $('#offcanvas-admin-sidebar .admin-edit-category-description').click(function() {
         // Replace the description with input field
         var description = $('.category-description').html().trim();
+        var name = $('.category-title').html().trim();
         $('.category-description').html('<input type="text" value="' + description + '">');
+        $('.category-header .category-title').html('<input type="text" style="text-align: center" value="' + name + '">');
     
         // Toggle the button
         $('#offcanvas-admin-sidebar .admin-edit-category-description-submit').removeClass('hidden');
@@ -14,6 +16,7 @@ $(function() {
     $('#offcanvas-admin-sidebar .admin-edit-category-description-submit').click(function() {
         // Replace the description with input field
         var new_description = $('.category-description > input').val().trim();
+        var new_category_name = $('.category-header .category-title > input').val().trim();
         var category_alias = $('.category-description').data('category-alias');
 
         $.ajax({
@@ -21,6 +24,7 @@ $(function() {
             type: 'POST',
             data: {
                 category_alias: category_alias,
+                new_title: new_category_name,
                 new_description: new_description,
             },
             success: function ( data ) {
@@ -28,9 +32,10 @@ $(function() {
                     console.log('success');
                     // Remove the input fields and replace with new text
                     $('.category-description').html( new_description );
+                    $('h1.category-title').html( new_category_name );
                     // Toggle the button
                     $('#offcanvas-admin-sidebar .admin-edit-category-description-submit').addClass('hidden');
-                    $('#offcanvas-admin-sidebar .admin-edit-category-description').removeClass('hidden');            
+                    $('#offcanvas-admin-sidebar .admin-edit-category-description').removeClass('hidden');
                 }
             }
         })
