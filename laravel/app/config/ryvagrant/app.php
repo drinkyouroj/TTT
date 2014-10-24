5763ceb111ce83f58801c2ecee9c9d8cc4013db4
Ryuhei Yokokawa <ryuhei@y-designs.com>
2014-10-24 04:48:25 +0000

    added ryvagrant a vagrant config set for ryuheis mbp

---
+++ laravel/app/config/ryvagrant/app.php
@@ -0,0 +1,9 @@
+<?php
+
+return array(
+	'url' => 'http://localhost:4567',
+	'imageurl' => 'http://localhost:4567/uploads/final_images',
+	'staticurl' => '//localhost:4567',
+	'email_send' => false,
+	'enable_ssl' => false,//enables SSL force on the user and myprofile routes.
+);
