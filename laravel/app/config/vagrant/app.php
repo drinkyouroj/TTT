febcf155249822334680449cd0433f6da31de3e5
Ryuhei Yokokawa <ryuhei@y-designs.com>
2014-10-27 22:43:22 +0000

    post actions user links and user dropdown fixes

--- laravel/app/config/vagrant/app.php
+++ laravel/app/config/vagrant/app.php
@@ -1,9 +1,9 @@
 <?php
 
 return array(
-	'url' => 'http://192.168.9.130',
-	'imageurl' => 'http://192.168.9.130/uploads/final_images',
-	'staticurl' => '//192.168.9.130',
+	'url' => 'http://192.168.9.131',
+	'imageurl' => 'http://192.168.9.131/uploads/final_images',
+	'staticurl' => '//192.168.9.131',
 	'email_send' => true,
 	'enable_ssl' => false,//enables SSL force on the user and myprofile routes.
 );
