13c32de68291c1a6f4089e35cb8c9f8a5efb3cd9
nharlow89 <nharlow89@gmail.com>
2014-10-30 13:22:50 -0700

    checking for error

--- laravel/app/views/v2/posts/post.blade.php
+++ laravel/app/views/v2/posts/post.blade.php
@@ -231,14 +231,7 @@
 					</div>
 
 					<ul class="post-categories list-inline">
-						@for ($i = 0; $i < count($post->categories); $i++)
-							<li> 
-								<a href="{{ URL::to( 'categories/'.$post->categories[$i]->alias ) }}"> {{ strtoupper( $post->categories[$i]->title ) }} </a> 
-								@if ( $i != count($post->categories) - 1 )
-									/
-								@endif
-							</li>
-						@endfor
+						
 					</ul>
 
 				</div>
