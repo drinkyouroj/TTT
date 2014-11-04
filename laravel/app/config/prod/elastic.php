6e0bf2b732cecf40a4727aec670a342288335d52
Ryuhei Yokokawa <ryuhei@y-designs.com>
2014-11-03 16:32:13 -0800

    elasticsearch ip change

--- laravel/app/config/prod/elastic.php
+++ laravel/app/config/prod/elastic.php
@@ -1,5 +1,5 @@
 <?php
-define('ES_HOST', '54.201.159.191');
+define('ES_HOST', '10.0.0.228');
 return array(
 	'hosts' => array(
 		ES_HOST.":9200",
--- laravel/app/views/v2/layouts/master.blade.php
+++ laravel/app/views/v2/layouts/master.blade.php
@@ -116,7 +116,7 @@
     
 	
 	
-	@if($app->environment() == 'ec2-cluster')
+	@if($app->environment() == 'ec2-cluster' || $app->environment() == 'prod')
 	<script>
 	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
